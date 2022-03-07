1ab6892b1dd340951724345d96fd986c313564e1
IyerAshwinKumar <61666069+IyerAshwinKumar@users.noreply.github.com>
2022-03-07 22:07:17 +0100

    Add files via upload

--- conftest.py
+++ conftest.py
@@ -1,19 +1,55 @@
 import pytest
+from selenium import webdriver
+import time
+driver = None
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
 
 
 @pytest.fixture(scope="class")
-def setup():
-    print("I will be executing first")
+def setup(request):
+    global driver
+    browser_name=request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
+    elif browser_name == "IE":
+        print("IE driver")
+    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.maximize_window()
+
+    request.cls.driver = driver
     yield
-    print(" I will execute last")
+    driver.close()
 
 
-@pytest.fixture()
-def dataLoad():
-    print("user profile data is being created")
-    return ["Rahul","Shetty","rahulshettyacademy.com"]
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
 
 
-@pytest.fixture(params=[("chrome","Rahul","shetty"), ("Firefox","shetty"), ("IE","SS")])
-def crossBrowser(request):
-    return request.param
+def _capture_screenshot(name):
+        driver.get_screenshot_as_file(name)
+
