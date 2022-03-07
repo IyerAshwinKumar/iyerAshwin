819070eebfd7186e840b26905fc8ec05baa68e4b
IyerAshwinKumar <61666069+IyerAshwinKumar@users.noreply.github.com>
2022-03-07 22:04:37 +0100

    Add files via upload

--- BaseClass.py
+++ BaseClass.py
@@ -1,8 +1,16 @@
 import inspect
 import logging
 
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 
+
+@pytest.mark.usefixtures("setup")
 class BaseClass:
+
     def getLogger(self):
         loggerName = inspect.stack()[1][3]
         logger = logging.getLogger(loggerName)
@@ -15,4 +23,10 @@ class BaseClass:
         logger.setLevel(logging.DEBUG)
         return logger
 
+    def verifyLinkPresence(self, text):
+        element = WebDriverWait(self.driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, text)))
 
+    def selectOptionByText(self,locator,text):
+        sel = Select(locator)
+        sel.select_by_visible_text(text)
